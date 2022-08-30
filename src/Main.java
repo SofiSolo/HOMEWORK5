d627421217851406519a73f6c2b018b09cb2b06e
SofiSolo <sofa.galaxia@yandex.ru>
2022-08-30 16:19:44 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Домашнее задание 5");
+
+        //Задание 1
+
+        int age = 16;
+        if (age >= 18) {
+            System.out.println("Ура! Поздравляем, ты совершеннолетний!");}
+        if (age < 18)
+        {
+            System.out.println(" Подрасти ещё немного!");}
+        }
+
+
+
+
+
+
+
+
+
+}
+
